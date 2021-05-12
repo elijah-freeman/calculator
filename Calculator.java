37c9ab631757f1ea6418b65f170152710fd353c0
Elijah Freeman <elijah@elijahfreeman.com>
2021-05-12 08:40:03 -0700

    Created Calculator.java file to define calculator class

---
+++ Calculator.java
@@ -0,0 +1,7 @@
+/*H*****************************************************************************
+ * Filename: Calculator.java
+ * Description: Defines a calculator class 
+ * Comment:
+ * Modified: 2021-05-11	File Created.
+ * Author: Elijah Freeman (elijah@elijahfreeman.com, elijahfreeman.com)
+ ****************************************************************************H*/
